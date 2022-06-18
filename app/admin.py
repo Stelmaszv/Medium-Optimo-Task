9b75faf7f5a9f69a823ee9d4ec1a80950f5270be
= <stelmaszv@gmail.com>
2022-06-18 13:56:37 +0200

    Add register models for admin view

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.models import Article, Author, Comments, Tag
+
+admin.site.register(Article)
+admin.site.register(Author)
+admin.site.register(Tag)
+admin.site.register(Comments)
