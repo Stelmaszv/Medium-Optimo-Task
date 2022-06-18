f049bed0e3356054745ea9c52e6753d91cbc3e3c
= <stelmaszv@gmail.com>
2022-06-18 12:22:00 +0200

    Add Model Tag relation

--- app/models.py
+++ app/models.py
@@ -13,17 +13,25 @@ def url_validator(url):
         raise ValidationError('Url is invalid !')
 
 class Author(models.Model):
-    name    = models.CharField(max_length=100,null=True,blank=True)
-    surname = models.CharField(max_length=100, null=True, blank=True)
-    User = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
+    name      = models.CharField(max_length=100,null=True,blank=True)
+    surname   = models.CharField(max_length=100, null=True, blank=True)
+    User      = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return  self.name+' '+self.surname
 
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=100,null=True,blank=True)
+    create_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return  self.tag_name
+
 class Article(models.Model):
     title     = models.CharField(max_length=200, null=True, blank=False)
     content   = models.TextField(default='', null=True, blank=False)
-    Author = models.ForeignKey(Author, on_delete=models.CASCADE, blank=True, null=True)
+    Author    = models.ForeignKey(Author, on_delete=models.CASCADE, blank=True, null=True)
+    Tags      = models.ManyToManyField(to='app.Tag', related_name='Tags', blank=True)
     url       = models.CharField(max_length=200, null=True, blank=True,validators=[url_validator])
     photo     = models.ImageField(upload_to='images', null=True, blank=True)
     create_at = models.DateTimeField(auto_now=True)
