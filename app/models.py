a5dc91c2b38242760c1c8a1bb686a6e98f7690c5
= <stelmaszv@gmail.com>
2022-06-18 12:26:16 +0200

    Add Model Comments and relation

--- app/models.py
+++ app/models.py
@@ -32,9 +32,23 @@ class Article(models.Model):
     content   = models.TextField(default='', null=True, blank=False)
     Author    = models.ForeignKey(Author, on_delete=models.CASCADE, blank=True, null=True)
     Tags      = models.ManyToManyField(to='app.Tag', related_name='Tags', blank=True)
+    Comments = models.ManyToManyField(to='app.Comments', related_name='Comments', blank=True)
     url       = models.CharField(max_length=200, null=True, blank=True,validators=[url_validator])
     photo     = models.ImageField(upload_to='images', null=True, blank=True)
     create_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return  self.title
+
+class Comments(models.Model):
+    content  = models.TextField(default='', null=False, blank=False)
+    Author   = models.ForeignKey(Author, on_delete=models.CASCADE, blank=False, null=True)
+    Article  = models.ForeignKey(Article, on_delete=models.CASCADE, blank=False, null=False)
+    create_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        super(Comments, self).save()
+        self.Article.Comments.add(self)
+
+    def __str__(self):
+        return  str(self.Author)+' - '+self.content
