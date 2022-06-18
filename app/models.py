8130b4835d1deac215b5c154c0b68e713b627eef
= <stelmaszv@gmail.com>
2022-06-18 12:20:35 +0200

    Add Model Author

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.exceptions import ValidationError
+from django.conf.global_settings import AUTH_USER_MODEL
 import validators
 
 def url_validator(url):
@@ -11,9 +12,18 @@ def url_validator(url):
     if url.find("youtube.com")==-1:
         raise ValidationError('Url is invalid !')
 
+class Author(models.Model):
+    name    = models.CharField(max_length=100,null=True,blank=True)
+    surname = models.CharField(max_length=100, null=True, blank=True)
+    User = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return  self.name+' '+self.surname
+
 class Article(models.Model):
     title     = models.CharField(max_length=200, null=True, blank=False)
     content   = models.TextField(default='', null=True, blank=False)
+    Author = models.ForeignKey(Author, on_delete=models.CASCADE, blank=True, null=True)
     url       = models.CharField(max_length=200, null=True, blank=True,validators=[url_validator])
     photo     = models.ImageField(upload_to='images', null=True, blank=True)
     create_at = models.DateTimeField(auto_now=True)
